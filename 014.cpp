d51ec4b6db52dcd6a34a35e13f1752975e3e2165
cxxxin <2609409586@qq.com>
2022-03-16 21:09:06 +0800

    three problems today

--- 014.cpp
+++ 014.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,15 +12,40 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans;
+        string ans = "";
 
+        int num = strs.size();
+        int* length = new int [num];
+        for(int i=0;i<num;i++){
+            length[i] = strs[i].size();
+        }
+        int minlen = *min_element(length,length+num);
+        
+        for(int i=0;i<minlen;i++){
+            //comparison
+            bool mark = true;
+            for(int j = 1;j<num;j++){
+                if(strs[j][i]!=strs[0][i]){
+                    mark = false;
+                    break;
+                }
+            }
+            if (mark == true){
+                ans += strs[0][i];
+            }
+            else{
+                break;
+            }
+        }
+        delete [] length;
         return ans;
     }
 };
 
 int main(){
     Solution s;
-    string result = s.longestCommonPrefix(["test","testtest"]);
+    vector<string> example = {"flower","flow","flight"};
+    string result = s.longestCommonPrefix(example);
     cout<<result<<endl;
     system("pause");
     return 0;
