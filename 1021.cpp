c72f7fa9e53abb8b168bb417c6ffc48e10c28fe8
cxxxin <2609409586@qq.com>
2022-05-28 22:27:05 +0800

    daily

---
+++ 1021.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+/*
+删除最外层的括号
+*/
+
+/*
+思路：
+    栈从空到下一次空的过程，则是扫描了一个原语的过程
+*/
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        stack<char> st;
+        for(char c:s){
+            if(c==')'){
+                st.pop();
+            }
+            if(!st.empty()){ // 不是原语
+                ans.push_back(c);
+            }
+            if(c=='('){
+                st.emplace(c);
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    return 0;
+}
