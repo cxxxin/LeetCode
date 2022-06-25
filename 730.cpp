cab4c8e72bad2f4369833b388d462fa88d76e958
cxxxin <2609409586@qq.com>
2022-06-25 14:17:20 +0800

    dp

--- 730.cpp
+++ 730.cpp
@@ -16,17 +16,64 @@ using namespace std;
 
 /*
 思路：
-    
+
 */
 
 class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    int countPalindromicSubsequences(string &s) {
-        
+    int countPalindromicSubsequences(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        vector<vector<int>> next(n, vector<int>(4));
+        vector<vector<int>> pre(n, vector<int>(4));
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        vector<int> pos(4, -1);
+
+        for (int i = 0; i < n; i++) {
+            for (int c = 0; c < 4; c++) {
+                pre[i][c] = pos[c];
+            }
+            pos[s[i] - 'a'] = i;
+        }
+
+        pos[0] = pos[1] = pos[2] = pos[3] = n;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int c = 0; c < 4; c++) {
+                next[i][c] = pos[c];
+            }
+            pos[s[i] - 'a'] = i;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    int low = next[i][s[i] - 'a'];
+                    int high = pre[j][s[i] - 'a'];
+                    if (low > high) {
+                        dp[i][j] = (2 + dp[i + 1][j - 1] * 2) % MOD;
+                    } else if (low == high) {
+                        dp[i][j] = (1 + dp[i + 1][j - 1] * 2) % MOD;
+                    } else {
+                        dp[i][j] = (0LL + dp[i + 1][j - 1] * 2 - dp[low + 1][high - 1] + MOD) % MOD;
+                    }
+                } else {
+                    dp[i][j] = (0LL + dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1] + MOD) % MOD;
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 };
+
 
 int main(){
     return 0;
