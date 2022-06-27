a6aee94cab6f560d6b8b93576aa7d1aa9006e28e
cxxxin <2609409586@qq.com>
2022-06-27 22:09:42 +0800

    weekly

--- 5229.cpp
+++ 5229.cpp
@@ -13,22 +13,25 @@ using namespace std;
 */
 
 class Solution {
-    int solve(vector<int> &nums1, vector<int> &nums2) {
-        int s1 = 0, maxSum = 0;
-        for (int i = 0, s = 0; i < nums1.size(); ++i) {
-            s1 += nums1[i];
-            s = max(s + nums2[i] - nums1[i], 0);
-            maxSum = max(maxSum, s);
-        }
-        return s1 + maxSum;
-    }
-
 public:
-    int maximumsSplicedArray(vector<int> &nums1, vector<int> &nums2) {
-        return max(solve(nums1, nums2), solve(nums2, nums1));
+    long long  solve(vector<int>& nums1, vector<int>& nums2)
+    {
+        long long  res = 0;
+        int n = nums1.size();
+        long long  mx = 0;
+        for (int i = 0; i < n; ++i) {
+            long long diff = nums2[i] - nums1[i];
+            mx = max(diff, mx + diff);
+            res = max(res, mx);
+        }
+        return res;
+    }
+    int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
+        long long tot1 = accumulate(nums1.begin(), nums1.end(), 0);
+        long long tot2 = accumulate(nums2.begin(), nums2.end(), 0);
+        return max(tot1 + solve(nums1, nums2), tot2 + solve(nums2, nums1));
     }
 };
-
 
 int main(){
     return 0;
