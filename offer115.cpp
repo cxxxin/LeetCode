44c037a95e1fd6037e313de893aeb217076f7e6e
cxxxin <2609409586@qq.com>
2022-07-24 00:04:04 +0800

    daily

--- offer115.cpp
+++ offer115.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_set>
 using namespace std;
 
 /*
@@ -16,33 +17,33 @@ using namespace std;
 class Solution {
 public:
     bool sequenceReconstruction(vector<int>& nums, vector<vector<int>>& sequences) {
-        int n = nums.size();
-        vector<int> indegrees(n + 1);
-        vector<unordered_set<int>> graph(n + 1);
+        int n = nums.size(); // 有n个节点
+        vector<int> indegrees(n + 1); // 记录每个节点的入度
+        vector<unordered_set<int>> graph(n + 1); // 建立有向图
         for (auto &sequence : sequences) {
             for (int i = 1; i < sequence.size(); i++) {
                 int prev = sequence[i - 1], next = sequence[i];
                 if (!graph[prev].count(next)) {
-                    graph[prev].emplace(next);
-                    indegrees[next]++;
+                    graph[prev].emplace(next); // 加一条边
+                    indegrees[next]++; // 入度++
                 }
             }
         }
-        queue<int> qu;
+        queue<int> qu; // 用来选择下一个节点
         for (int i = 1; i <= n; i++) {
-            if (indegrees[i] == 0) {
+            if (indegrees[i] == 0) { // 入度为0
                 qu.emplace(i);
             }
         }
         while (!qu.empty()) {
-            if (qu.size() > 1) {
+            if (qu.size() > 1) { // 有不止一个选择
                 return false;
             }
-            int num = qu.front();
+            int num = qu.front(); // 新的起点
             qu.pop();
-            for (int next : graph[num]) {
-                indegrees[next]--;
-                if (indegrees[next] == 0) {
+            for (int next : graph[num]) { // 找下一个可去的节点
+                indegrees[next]--; // 入度--
+                if (indegrees[next] == 0) { // 入度为0就可以成为新的起点了
                     qu.emplace(next);
                 }
             }
