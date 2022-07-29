528c5f6314d5ad461d3d88aca6289e862b08af75
cxxxin <2609409586@qq.com>
2022-07-29 12:44:01 +0800

    t拓扑排序

--- 207.cpp
+++ 207.cpp
@@ -21,9 +21,39 @@ using namespace std;
 */
 
 class Solution {
+private:
+    vector<vector<int>> edges;
+    vector<int> indeg; // 入度
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        indeg.resize(numCourses);
+        for(auto& info:prerequisites){
+            edges[info[1]].push_back(info[0]); // 由info1指向info0的边
+            indeg[info[0]]++;
+        }
 
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+
+        int visited = 0;
+        while(!q.empty()){
+            visited++;
+            int curr = q.front();
+            q.pop();
+            for(int next:edges[curr]){
+                indeg[next]--;
+                if(indeg[next]==0){
+                    q.push(next);
+                }
+            }
+        }
+
+        return visited == numCourses;
     }
 };
 
